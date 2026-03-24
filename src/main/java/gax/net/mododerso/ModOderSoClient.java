81aee32d13142e062821c2c3d20e9da8c453d9bf
darkf <darkfinst7567@posteo.de>
2026-03-24 22:26:34 +0100

    Initial commit

---
+++ src/main/java/gax/net/mododerso/ModOderSoClient.java
@@ -0,0 +1,10 @@
+package gax.net.mododerso;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModOderSoClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/gax/net/mododerso/ModOderSoDataGenerator.java
@@ -0,0 +1,11 @@
+package gax.net.mododerso;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ModOderSoDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
